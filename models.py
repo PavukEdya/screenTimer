402832881c4aec5c562456791e87983b04b2a931
PavukEdya <slaefir@mail.ru>
2024-01-12 23:23:40 +0500

    create database representations to window and session

--- models.py
+++ models.py
@@ -1,15 +1,18 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, TIMESTAMP, Time
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import Column, ForeignKey, Integer, String, TIMESTAMP, DateTime, CheckConstraint
+from sqlalchemy.orm import relationship, DeclarativeBase, declarative_base
+from sqlalchemy.sql import func
 
-Base = declarative_base()
+
+class Base(DeclarativeBase):
+    pass
 
 
 class BaseModel(Base):
     __abstract__ = True
 
-    id = Column(Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
-    created_at = Column(TIMESTAMP, nullable=False)
-    updated_at = Column(TIMESTAMP, nullable=False)
+    id = Column(Integer, primary_key=True)
+    created_at = Column(TIMESTAMP, default=func.now())
+    updated_at = Column(TIMESTAMP, default=func.now(), onupdate=func.now())
 
     def __repr__(self):
         return "<{0.__class__.__name__}(id={0.id!r})>".format(self)
@@ -18,14 +21,23 @@ class BaseModel(Base):
 class Window(BaseModel):
     __tablename__ = 'window'
 
-    name = Column(String)
-    fullname = Column(String)
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    fullname = Column(String, nullable=False)
+
+    sessions = relationship('Session', back_populates='window')
 
 
 class Session(BaseModel):
     __tablename__ = 'session'
 
-    window_id = ForeignKey('window.id')
-    time = Column(Time)
+    id = Column(Integer, primary_key=True)
+    window_id = Column(Integer, ForeignKey('window.id', ondelete='CASCADE'), nullable=False)
+    start_time = Column(DateTime, nullable=False)
+    end_time = Column(DateTime, nullable=False)
 
-    window = relationship('Window',back_populates='sessions')
+    window = relationship('Window', back_populates='sessions')
+
+    __table_args__ = (
+        CheckConstraint('start_time < end_time', name='start_end_check'),
+    )
